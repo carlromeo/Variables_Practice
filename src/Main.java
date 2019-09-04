6e542ee4366a487fff32c161d401f6c47de22e14
carlromeo <carlromeo.dogbey@gmail.com>
2019-09-04 16:54:36 -0400

    Getting_and_Storing_User_Input

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import static java.lang.Integer.MAX_VALUE;
+
+public class Main {
+    public static void main(String[] args){
+        int x, y, answer;
+        double GPA;
+        String firstName, lastName;
+        String question = "Black Panther";
+        x = 99;
+        y = MAX_VALUE;
+        answer=40;
+        firstName = "Carl Romeo";lastName = "Dogbey";
+       GPA = 3.46;
+
+        System.out.println("I go by " + firstName + " " + lastName);
+        System.out.println("My age is: " + answer);
+        System.out.println("The last movie I saw was" + question);
+        System.out.println("My last known GPA was "
+                + GPA);
+
+    }
+}
